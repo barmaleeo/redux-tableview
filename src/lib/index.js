b3a8b23f2b944d0408ce697f5feae91f1a18f7cb
pavel <s20305382@gmail.com>
2019-04-02 15:51:12 +0300

    Initial commit

--- src/lib/index.js
+++
@@ -1,7 +0,0 @@
-'use-strict';
-
-import tableViewActions from "./tableViewActions";
-import TableViewComponent from "./tableViewComponent";
-import tableViewReducer from "./tableViewReduser";
-import tableViewConstants from "./tableviewConstants";
-export { tableViewActions, TableViewComponent, tableViewReducer,tableViewConstants };
